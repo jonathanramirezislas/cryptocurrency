53445e7fa83eee03a40107b6f7158d189c6d1e8a
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-02-12 16:07:58 -0600

    header
    Add fonts

---
+++ components/Form.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+export const Form = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, StyleSheet, Platform } from 'react-native';
+
+const Header = () => (
+
+    <Text style={styles.header}>Criptomonedas</Text>
+ );
+
+const styles = StyleSheet.create({
+    header: {
+        paddingTop: Platform.OS === 'ios' ? 50 : 10,
+        fontFamily: 'Lato-Black',
+        backgroundColor: '#5E49E2',
+        paddingBottom: 10,
+        textAlign: 'center',
+        textTransform: 'uppercase',
+        fontSize: 20,
+        color: '#FFF',
+        marginBottom: 30
+    }
+})
+ 
+export default Header;
